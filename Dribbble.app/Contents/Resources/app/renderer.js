99a58f588c18c8b673ee29b8836a41262e342725
Kieron Keenan <kieronkeenan@Kierons-MacBook-Pro-2.local>
2017-08-27 23:49:24 +0100

    adding app and docs

---
+++ Dribbble.app/Contents/Resources/app/renderer.js
@@ -0,0 +1,21 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+var request = require('request');
+
+
+
+
+  // setInterval (function(){
+  //     request("https://finance.google.com/finance/info?client=ig&q=goog", function(error, response, body) {
+  //     body = body.slice(3);
+  //     body = JSON.parse(body);
+  //     console.log(body);
+  //     newPrice(body);
+  //   });
+  // },1000);
+
+  // function newPrice(arr) {
+  //    currentPrice = arr[0]["l"];
+  //    document.getElementById("price").innerHTML = currentPrice;
+  // }
